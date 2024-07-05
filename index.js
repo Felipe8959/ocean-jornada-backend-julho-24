df491a0e8d994cbb853db215ce5a5356dc2949b5
Felipe89592 <felipe.santos369@hotmail.com>
2024-07-05 09:04:36 -0300

    Conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,21 +1,70 @@
 const express = require('express')
+const { MongoClient } = require('mongodb')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const dbUrl = 'mongodb+srv://admin:fbJkGwsGu8uMmb2m@cluster0.sicvr2n.mongodb.net'
+const dbName = 'ocean-jornada-backend'
+const client = new MongoClient(dbUrl)
 
-app.get('/oi', function (req, res) {
-    res.send('Olá, mundo!')
+
+async function main() {
+  console.log('Conectando ao banco de dados...')
+  client.connect()
+  console.log('Conexão realizada')
+
+  app.get('/', function (req, res) {
+    res.send('Hello World')
   })
-  
 
-// lista de personangens
-const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+  app.get('/oi', function (req, res) {
+      res.send('Olá, mundo!')
+    })
+    
 
-// read all
-app.get('/item', function (req, res) {
-  res.send(lista)
-})
+  // lista de personangens
+  const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
 
-app.listen(3000)
+
+  // read all
+  app.get('/item', function (req, res) {
+    res.send(lista)
+  })
+
+  // sinalizamos para o express que iremos utilizar json no body
+  app.use(express.json())
+
+  // create
+  app.post('/item', function (req, res) {
+    console.log(req.body)
+    const item = req.body.nome
+    console.log(item)
+
+    lista.push(item)
+
+    res.send(lista)
+  })
+
+
+  // Read by id [GET]
+  app.get('/item/:id', function (req, res) {
+    const id = req.params.id
+    const item = lista[id - 1]
+    res.send(item)
+  })
+
+
+  // Update [PUT]
+  app.put('/item/:id', function (req, res) {
+    const id = req.params.id
+    const novoItem = req.body.nome
+
+    lista[id - 1] = novoItem
+
+    res.send('Item atualizado ' + id)
+  })
+
+
+  app.listen(3000)
+}
+
+main()
